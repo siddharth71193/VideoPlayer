7526d56ec74b249f979cb38abdd7758a5131d3c7
siddharth711931 <siddharth.rajan@sustlabs.com>
2021-06-13 11:29:40 +0530

    Constants added

---
+++ app/src/main/java/com/example/videosample1/util/Constants.java
@@ -0,0 +1,10 @@
+package com.example.videosample1.util;
+
+public class Constants {
+    public class IntentExtraKeys {
+        public static final String VIDEO_URL = "video_url";
+        public static final String SEEKBAR_POSITION = "seekbar_position";
+        public static final String VIDEO_TRANSITION_NAME = "video_transition_name";
+        public static final String VIDEO_DESCRIPTION = "video_description";
+    }
+}
